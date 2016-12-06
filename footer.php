065d32aaee2dcca5935315bf997a062808781f87
ihewro <ihewro@163.com>
2016-12-06 20:34:19 +0800

    微调网站统计代码带来的样式

--- footer.php
+++ footer.php
@@ -2,11 +2,11 @@
 
   <footer id="footer" class="app-footer" role="footer">
     <div class="wrapper b-t bg-light">
-      <span class="pull-right hidden-xs">Power by <a data-no-instant target="blank" href="http://www.typecho.org">Typecho</a> | Theme <a data-no-instant target="blank" href="http://www.ihewro.com">handsome</a> <a href ui-scroll="app" class="m-l-sm text-muted"><i class="fa fa-long-arrow-up"></i></a>
   <!--网站统计代码-->
 <script data-no-instant type="text/javascript">
   <?php $this->options->analysis(); ?>
 </script>
+      <span class="pull-right hidden-xs">Power by <a data-no-instant target="blank" href="http://www.typecho.org">Typecho</a> | Theme <a data-no-instant target="blank" href="http://www.ihewro.com">handsome</a> <a href ui-scroll="app" class="m-l-sm text-muted"><i class="fa fa-long-arrow-up"></i></a>
       </span>
       &copy; <?php echo date("Y");?> Copyright.
     </div>
