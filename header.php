a86a69de2f1f7f6118303f1e7effaa2029d50092
ihewro <ihewro@163.com>
2017-02-06 14:26:34 +0800

    优化头部信息输出顺序
    fix #15

--- header.php
+++ header.php
@@ -3,39 +3,41 @@
 <!DOCTYPE HTML>
 <html class="no-js" <?php if($this->options->langis == '0'): ?>lang="en"<?php elseif($this->options->langis == '1'): ?>lang="zh-cmn-Hans"<?php elseif($this->options->langis == '2'): ?>lang="zh-cmn-Hant"<?php endif; ?>>
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-    <meta charset="<?php $this->options->charset(); ?>">
-    <!--IE 8浏览器的页面渲染方式-->
-    <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
-    <!--默认使用极速内核：针对国内浏览器产商-->
-    <meta name="renderer" content="webkit">
-    <!--针对移动端的界面优化-->
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <!--chrome Android 地址栏颜色-->
-    <?php if($this->options->ChromeThemeColor): ?>
-    <meta name="theme-color" content="<?php $this->options->ChromeThemeColor() ?>" />
-    <?php endif; ?>
-    <title><?php $this->archiveTitle(array(
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+<meta charset="<?php $this->options->charset(); ?>">
+<!--IE 8浏览器的页面渲染方式-->
+<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
+<!--默认使用极速内核：针对国内浏览器产商-->
+<meta name="renderer" content="webkit">
+<!--针对移动端的界面优化-->
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<!--chrome Android 地址栏颜色-->
+<?php if($this->options->ChromeThemeColor): ?>
+<meta name="theme-color" content="<?php $this->options->ChromeThemeColor() ?>" />
+<?php endif; ?>
+<title><?php $this->archiveTitle(array(
             'category'  =>  _t('分类 %s 下的文章'),
             'search'    =>  _t('包含关键字 %s 的文章'),
             'tag'       =>  _t('标签 %s 下的文章'),
             'author'    =>  _t('%s 发布的文章')
-        ), '', ' - '); ?><?php $this->options->title(); ?><?php if ($this->is('index')) : ?> - <?php $this->options->titleintro() ?><?php endif; ?></title>
-    <?php if($this->options->favicon): ?>
-    <link rel="icon" type="image/ico" href="<?php $this->options->favicon() ?>">
-    <?php endif; ?>
+        ), '', ' - '); ?><?php $this->options->title(); ?><?php if ($this->is('index')) : ?> - <?php $this->options->titleintro() ?><?php endif; ?>
+</title>
+<?php $this->header(); ?>
+<?php if($this->options->favicon): ?>
+<link rel="icon" type="image/ico" href="<?php $this->options->favicon() ?>">
+<?php endif; ?>
 
 
-    <!-- 第三方CDN加载CSS -->
-    <link href="//cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-    <script src="//cdn.bootcss.com/jquery/2.1.4/jquery.min.js" data-no-instant></script>
-    <!-- 本地develope版本 -->
-  <!--
-  <link rel="stylesheet" href="<?php $this->options->themeUrl('css/animate.css') ?>" type="text/css" />
-  <link rel="stylesheet" type="text/css" href="<?php $this->options->themeUrl('css/iconfont.css') ?>">
-  <link rel="stylesheet" href="<?php $this->options->themeUrl('css/font.css') ?>" type="text/css" />
-  <link rel="stylesheet" href="<?php $this->options->themeUrl('css/OwO.min.css') ?>" type="text/css" />
-  <link rel="stylesheet" href="<?php $this->options->themeUrl('css/app.css') ?>" type="text/css" />-->
+<!-- 第三方CDN加载CSS -->
+<link href="//cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+<script src="//cdn.bootcss.com/jquery/2.1.4/jquery.min.js" data-no-instant></script>
+<!-- 本地develope版本 -->
+<!--
+<link rel="stylesheet" href="<?php $this->options->themeUrl('css/animate.css') ?>" type="text/css" />
+<link rel="stylesheet" type="text/css" href="<?php $this->options->themeUrl('css/iconfont.css') ?>">
+<link rel="stylesheet" href="<?php $this->options->themeUrl('css/font.css') ?>" type="text/css" />
+<link rel="stylesheet" href="<?php $this->options->themeUrl('css/OwO.min.css') ?>" type="text/css" />
+<link rel="stylesheet" href="<?php $this->options->themeUrl('css/app.css') ?>" type="text/css" />-->
 
 
 
@@ -187,8 +189,6 @@
                 <?php endif; ?>
   }
 </style>
-    <!-- 通过自有函数输出HTML头部信息 -->
-    <?php $this->header(); ?>
     
   <!--网站统计代码-->
 <script data-no-instant type="text/javascript">
